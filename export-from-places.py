44a17f38941f11f0bf973a644de2a4e2c550b0a0
Bill Melvin <bill@billmelvin.com>
2021-06-06 18:50:47 -0400

    Format output and write to 'output' subdir.

--- export-from-places.py
+++ export-from-places.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python3
 
 import sqlite3
-import csv 
 from pathlib import Path
+from datetime import datetime
 
 db_filename = "./data/places.sqlite"
+
+outpath_history_places = Path.cwd() / 'output' / 'history_places.csv'
 
 db_path = Path(db_filename)
 if db_path.exists():
     print(f"Reading {db_path}")
     connection = sqlite3.connect(db_path)
     c = connection.cursor()
-    
-    #c.execute("SELECT DISTINCT url FROM moz_places")
-    #print(c.fetchall())
-    
-    #sql = "SELECT a.title, b.url FROM moz_bookmarks a JOIN moz_places b ON b.id = a.fk"
-    
-    #sql = "SELECT p.title, a.title, b.url FROM (moz_bookmarks a JOIN moz_places b ON b.id = a.fk) JOIN moz_bookmarks p ON p.id = a.parent"
-    
-    sql = "SELECT p.url, p.title, h.visit_date FROM moz_historyvisits h JOIN moz_places p ON p.id = h.place_id"
+        
+    sql = "SELECT p.url, h.visit_date FROM moz_historyvisits h JOIN moz_places p ON p.id = h.place_id ORDER BY h.visit_date"
     
     c.execute(sql)
     
-    #for a in c.fetchall():
-    #    print(a)  
-    
-    with open('test2.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(c.fetchall())
+    with open(outpath_history_places, 'w') as f:
+        f.write("url,visit_date\n")
+        for row in c.fetchall():
+            url = row[0]
+            
+            # TODO: Probably don't want to truncate the URL at only 80 chars in final version.
+            if len(url) > 80:
+                url = url[:77] + '...'
+                
+            # The date values in the table are in microseconds since 
+            # the Unix epoch. Convert to seconds.
+            dts = row[1] / 1000000
+            
+            f.write('"{0}","{1}"{2}"'.format(
+                url,
+                datetime.fromtimestamp(dts),
+                "\n"
+            ))
     
     connection.close()
 else:
