1c66ebefcf6543584ca480cd6d3a444c85489f5a
Bill Melvin <bill@billmelvin.com>
2021-06-06 13:41:21 -0400

    Open database and run initial query.

--- export-from-places.py
+++ export-from-places.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-print("Here I am.")
+import sqlite3
+from pathlib import Path
+
+db_filename = "./data/places.sqlite"
+
+db_path = Path(db_filename)
+if db_path.exists():
+    print(f"Reading {db_path}")
+    connection = sqlite3.connect(db_path)
+    c = connection.cursor()
+    c.execute("SELECT DISTINCT url FROM moz_places")
+    print(c.fetchall())
+    connection.close()
+else:
+    print(f"ERROR: Cannot find {db_path}")
+
+print("Done.")
