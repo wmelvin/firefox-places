6a2f65482379fc4d3b4cf8ab79f1d5671093b810
Bill Melvin <bill@billmelvin.com>
2021-06-06 13:56:12 -0400

    Expand first query.

--- export-from-places.py
+++ export-from-places.py
@@ -10,8 +10,19 @@ if db_path.exists():
     print(f"Reading {db_path}")
     connection = sqlite3.connect(db_path)
     c = connection.cursor()
-    c.execute("SELECT DISTINCT url FROM moz_places")
-    print(c.fetchall())
+    
+    #c.execute("SELECT DISTINCT url FROM moz_places")
+    #print(c.fetchall())
+    
+    #sql = "SELECT a.title, b.url FROM moz_bookmarks a JOIN moz_places b ON b.id = a.fk"
+    
+    sql = "SELECT p.title, a.title, b.url FROM (moz_bookmarks a JOIN moz_places b ON b.id = a.fk) JOIN moz_bookmarks p ON p.id = a.parent"
+    
+    c.execute(sql)
+    for a in c.fetchall():
+        print(a)  
+    
+    
     connection.close()
 else:
     print(f"ERROR: Cannot find {db_path}")
