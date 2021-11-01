e198483c16d5f070e5314feea4bc810ad93b56c1
Bill Melvin <bill@billmelvin.com>
2021-11-01 11:44:26 -0400

    Build out HTML parsability check using html5lib.html5parser.

--- check_html_output.py
+++ check_html_output.py
@@ -1,32 +1,73 @@
 #!/usr/bin/env python3
 
+import sys
+
+from html5lib.html5parser import HTMLParser, ParseError
 from pathlib import Path
 
-import html5lib
+
+#  Note: When a ParseError occurs, the error messages do not give much
+#  context.  Running the tidy (http://www.html-tidy.org/) console
+#  application separately on a file that will not parse can provide
+#  clues to what needs fixed.
 
 
-def check_html(file_name):
-    print(file_name)
+def check_html(file_name, expect_footer):
+    print(f"\nParsing '{file_name}'")
 
     html = Path(file_name).read_text()
 
-    parser = html5lib.HTMLParser(strict=True)
+    parser = HTMLParser(strict=True)
     # parser = html5lib.HTMLParser()
 
-    document = parser.parse(html)
+    try:
+        document = parser.parse(html)
+    except ParseError as e:
+        print(f"  ERROR: {e}")
+        return False
+
+    if not expect_footer:
+        return True
 
     check = document.findall(".//*[@id='footer']")
-    assert len(check) == 1
+    if len(check) == 1:
+        return True
+    print('  ERROR: Element not found with id="footer".')
+    return False
 
 
-def main():
-    check_path = Path.cwd() / "output"
+def main(argv):
+    if len(argv) == 2:
+        #  Can take one argument that is the path containing HTML files.
+        check_path = Path(argv[1])
+        if not check_path.exists():
+            print(f"Path not found: '{check_path}'")
+            return 1
+    else:
+        check_path = Path.cwd() / "output"
+
     html_files = list(check_path.glob("**/*.html"))
+
+    if len(html_files) == 0:
+        print(f"No files found matching *.html in '{check_path}'")
+        return 0
+
     html_files.sort()
+    any_errors = False
+
     for file in html_files:
-        if "PC_2-" in str(file):
-            check_html(file)
+        if check_html(file, expect_footer=True):
+            print("  Ok")
+        else:
+            any_errors = True
+
+    if any_errors:
+        print("\nERRORS FOUND: Review console output.\n")
+    else:
+        print("\nAll files parsed successfully.\n")
+
+    return 0
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
